a0bd8bc62e23f4d17a6b502cc7719be71c1bc7e7
admin <admin@iMac3.local>
2017-11-02 17:29:58 +0100

    Beginning js script

---
+++ js/slider.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // TODO : To manage side strip colors on portrait images, go to Vibrant.js (https://jariz.github.io/vibrant.js/)
+
+    $("button[data-action='next']").on("click", function () {
+        debugger;
+    });
+});
